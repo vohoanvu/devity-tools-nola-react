0a0e8a42abffe4a580ef0a077aaab516af98dfcf
Vu Vo <vohoanvu96@gmail.com>
2022-12-13 17:30:38 +0700

    fixed react warning

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -9,7 +9,7 @@ import btn_image_config from "../img/d_btn_ctrl_config.png";
 
 const Prompt = () => {
   const [data, setData] = useState({data: []});
-  const [isLoading, setIsLoading] = useState(false);
+  //const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState('');
 
   const fetchData = async () => {
@@ -21,25 +21,22 @@ const Prompt = () => {
 
     console.log('serach for ' + term);
   
-    if(term){
-    $("#prompt_input").val('');
-
-      let search_api = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAzgX2yArFJrRogwd5GCdkjmQaUwGUWMqs&cx=b2801acca79e24323&q=" + encodeURIComponent(term);
-      return await axios.get(search_api)
-          .then((res) => {
-              setData(res.data.items);
-              $('div[data-panel=RESULTS] .gear').removeClass('rotate');
-              console.log('serach for ' + term);
-            
-              
-      }).catch((err) => {
-        console.log(err);
-      })
-    }
-    else{
+    if(term) {
+      $("#prompt_input").val('');
+        let search_api = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAzgX2yArFJrRogwd5GCdkjmQaUwGUWMqs&cx=b2801acca79e24323&q=" + encodeURIComponent(term);
+        
+        return await axios.get(search_api)
+            .then((res) => {
+                setData(res.data.items);
+                $('div[data-panel=RESULTS] .gear').removeClass('rotate');
+                console.log('search for ' + term);    
+            }).catch((error) => {
+                setErr(error);
+                console.log(err);
+            })
+    } else {
       console.log('Attempt tp search without entering search term');
     }
-
 
   };
 
@@ -83,6 +80,8 @@ const Prompt = () => {
                     autocorrect="off"
                     autofocus="" 
                     role="combobox" 
+                    aria-controls="prompt_input"
+                    aria-expanded="true"
                     spellcheck="false" 
                     title="Search" 
                     aria-label="Search">
@@ -93,7 +92,7 @@ const Prompt = () => {
       </div>
       <div className='p-panel results hidden' data-panel='RESULTS'>
               <div className='p-chrome'>
-                <img  src={btn_image_config} className='gear' />
+                <img src={btn_image_config} className='gear' alt="devity gear"/>
                 <span className='p-title'>Dev-Search (Please up-vote useful results!)</span>
               </div>
 
@@ -102,7 +101,7 @@ const Prompt = () => {
                         return (
                             <li data-cacheId={value.cacheId}>
                               <span>[{value.displayLink}]</span><br></br>
-                              <a target='_blank' href={value.link}>{value.title}</a> 
+                              <a target='_blank' href={value.link} rel="noreferrer">{value.title}</a> 
                             <div>
                             <span dangerouslySetInnerHTML={{__html: value.htmlSnippet}} />
                         

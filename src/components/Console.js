35e53deb875783ef3b65fb0cb30df694fbe4a4e7
Vu Vo <vohoanvu96@gmail.com>
2022-12-22 22:28:22 +0700

    fixed compile error

--- src/components/Console.js
+++ src/components/Console.js
@@ -127,13 +127,6 @@ const Console = (props) =>
 
   };
 
-  function loadClient() {
-    gapi.client.setApiKey("YOUR_API_KEY");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-
   function handleKeyDown(e) {
 
     var key = e.key;

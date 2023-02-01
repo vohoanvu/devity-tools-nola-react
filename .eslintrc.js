766c42448593d95940914a0fc982955b6dc718ab
Vu Vo <vohoanvu96@gmail.com>
2023-02-02 02:23:43 +0700

    separated leslint config and new build

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    env: {
+        browser: true, // Browser global variables like `window` etc.
+        commonjs: true, // CommonJS global variables and CommonJS scoping.Allows require, exports and module.
+        es6: true, // Enable all ECMAScript 6 features except for modules.
+        jest: true, // Jest global variables like `it` etc.
+        node: true // Defines things like process.env when generating through node
+    },
+    extends: [
+        "react-app",
+        "react-app/jest",
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugins: [
+        "react",
+        "react-hooks",
+        "jsx-a11y",
+        "import"
+    ],
+    root: true, // For configuration cascading.
+    rules: {
+        "react/prop-types": 0,
+        "indent": 1,
+        "linebreak-style": [1, process.env.NODE_ENV === "prod" ? "unix" : "windows"],
+        "quotes": [1, "double"],
+        "no-debugger": 0
+    },
+    settings: {
+        react: {
+            version: "detect" // Detect react version
+        }
+    }
+};
